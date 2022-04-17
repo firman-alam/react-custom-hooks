19ad60c32bde31ce596d8da23905bbaf055ab90c
firman-alam <aladiat046@gmail.com>
2022-04-17 12:22:45 +0700

    React custom hooks, using useState - useEffect - useRef

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
+import useRandomJoke from "./useRandomJoke";
 
 function App() {
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const joke = useRandomJoke(firstName, lastName);
+
+  const generateJoke = (e) => {
+    e.preventDefault();
+
+    setFirstName(firstNameRef.current.value);
+    setLastName(lastNameRef.current.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+        <h1>Joke Generator</h1>
+
+        <h2>{joke}</h2>
+
+        <form>
+          <input placeholder="first name" ref={firstNameRef} />
+          <input placeholder="last name" ref={lastNameRef} />
+          <button onClick={generateJoke}>Generate Joke</button>
+        </form>
+      </center>
     </div>
   );
 }
